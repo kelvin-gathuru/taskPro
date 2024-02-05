21dced3a73fcf21d8f307c9e0b8e1162d63d3bef
kelvin-gathuru <kelvingathuru@students.uonbi.ac.ke>
2024-02-05 11:38:52 +0300

    kanban

--- src/app/views/components/tasks/kanban/kanban.component.ts
+++ src/app/views/components/tasks/kanban/kanban.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { Table } from 'primeng/table';
 import { ApiService } from 'src/app/views/service/apiService';
 
 @Component({
@@ -50,60 +49,15 @@ export class KanbanComponent implements OnInit {
 
         
     }
-    onDragStart(task: any): void {
-        task.isDragging = true;
-      }
-    
-      onDragEnd(task: any): void {
-        task.isDragging = false;
-        const payload = {
-            stageId: this.stage,
-            taskId: task.id,
-        };
-        this.apiService.updateTaskStage(payload).subscribe(
-            (data: any) => {
-              console.log("success");
-            },
-            (error) => {
-              console.error('Error fetching property data:', error);
-            }
-          );
-      }
-    
-      onDragOver(event: DragEvent, zone: any): void {
-        event.preventDefault();
-        const bottomTask = this.insertAboveTask(zone, event.clientY);
-        const curTask = this.el.nativeElement.querySelector('.is-dragging');
-    
-        if (!bottomTask) {
-          this.renderer.appendChild(zone, curTask);
-        } else {
-          this.renderer.insertBefore(zone, curTask, bottomTask);
-        }
-      }
-    
-      onDrop(event: DragEvent, zone: any): void {
-
-        this.stage= zone.id;
-        
-      }
-    
-      private insertAboveTask(zone: any, mouseY: number): any {
-        const els = zone.querySelectorAll('.task:not(.is-dragging)');
-    
-        let closestTask = null;
-        let closestOffset = Number.NEGATIVE_INFINITY;
-    
-        els.forEach((task: any) => {
-          const { top } = task.getBoundingClientRect();
-          const offset = mouseY - top;
-    
-          if (offset < 0 && offset > closestOffset) {
-            closestOffset = offset;
-            closestTask = task;
-          }
-        });
-    
-        return closestTask;
-      }
+    allowDrop(ev: any) {
+      ev.preventDefault();
+    }
+    drag(ev: any) {
+      ev.dataTransfer.setData("text", ev.target.id);
+    }
+    drop(ev: any) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    }
 }
